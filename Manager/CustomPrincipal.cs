4dd085cb332e04e7de2c03c8a3976f9f6fb591ae
12nikola <nikola.rakita98@gmail.com>
2022-12-29 19:41:05 +0100

    kontura zadatka

---
+++ Manager/CustomPrincipal.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Security.Cryptography.X509Certificates;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Manager
+{
+    public class CustomPrincipal:IPrincipal
+    {
+        WindowsIdentity identity = null;
+        public CustomPrincipal(WindowsIdentity windowsIdentity)
+        {
+            identity = windowsIdentity;
+        }
+
+        public IIdentity Identity
+        {
+            get { return identity; }
+        }
+
+        public bool IsInRole(string role)
+        {
+            Type x509IdentityType = identity.GetType();
+
+            FieldInfo certificateField = x509IdentityType.GetField("certificate", BindingFlags.Instance | BindingFlags.NonPublic);
+
+            X509Certificate2 certificate = (X509Certificate2)certificateField.GetValue(identity);
+
+            string name = certificate.SubjectName.Name;
+            string[] clientName = name.Split(';');
+            string[] parts = clientName[0].Split(';');
+            string[] roleName = parts[1].Split('=');
+
+            if(role.Equals(roleName[1]))
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
