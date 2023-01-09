880e3635c285532c8bdffb9ee7b0f1b0bc567a9d
12nikola <102314632+12nikola@users.noreply.github.com>
2023-01-09 12:04:19 +0100

    Add files via upload

--- Manager/AuditEvents.cs
+++ Manager/AuditEvents.cs
@@ -12,7 +12,14 @@ namespace Manager
     {
         AuthenticationSuccess=0,
         AuthorizationSuccess=1,
-        AuthorizationFailure=2
+        AuthorizationFailure=2,
+        ReadFromFileSuccess=3,
+        ReadFromFileFailed=4,
+        WriteInFileSuccess=5,
+        WriteInFileFailed=6,
+        AddSuccess=7,
+        ChangeSuccess=8,
+        PaySuccess=9
     }
     public class AuditEvents
     {
@@ -55,6 +62,62 @@ namespace Manager
             get
             {
                 return ResourceMgr.GetString(AuditEventTypes.AuthorizationFailure.ToString());
+            }
+        }
+
+        public static string ReadFromFileSuccess
+        {
+            get
+            {
+                return ResourceMgr.GetString(AuditEventTypes.ReadFromFileSuccess.ToString());
+            }
+        }
+
+        public static string ReadFromFileFailed
+        {
+            get
+            {
+                return ResourceMgr.GetString(AuditEventTypes.ReadFromFileFailed.ToString());
+            }
+        }
+
+        public static string WriteInFileSuccess
+        {
+            get
+            {
+                return ResourceMgr.GetString(AuditEventTypes.WriteInFileSuccess.ToString());
+            }
+        }
+
+        public static string WriteInFileFailed
+        {
+            get
+            {
+                return ResourceMgr.GetString(AuditEventTypes.WriteInFileFailed.ToString());
+            }
+        }
+
+        public static string AddSuccess
+        {
+            get
+            {
+                return ResourceMgr.GetString(AuditEventTypes.AddSuccess.ToString());
+            }
+        }
+
+        public static string ChangeSuccess
+        {
+            get
+            {
+                return ResourceMgr.GetString(AuditEventTypes.ChangeSuccess.ToString());
+            }
+        }
+
+        public static string PaySuccess
+        {
+            get
+            {
+                return ResourceMgr.GetString(AuditEventTypes.PaySuccess.ToString());
             }
         }
     }
