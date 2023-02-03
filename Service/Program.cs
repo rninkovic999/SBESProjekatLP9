20cb9fed756e916fcfce9e3f9170ae9823a63e6c
12nikola <nikola.rakita98@gmail.com>
2023-02-03 10:02:31 +0100

    zadatak

--- Service/Program.cs
+++ Service/Program.cs
@@ -1,10 +1,18 @@
-﻿using Contracts;
+﻿using Common;
+using Manager;
 using System;
 using System.Collections.Generic;
+using System.IdentityModel.Policy;
 using System.Linq;
+using System.Runtime.Serialization;
+using System.Security.Cryptography.X509Certificates;
+using System.Security.Principal;
 using System.ServiceModel;
+using System.ServiceModel.Description;
+using System.ServiceModel.Security;
 using System.Text;
 using System.Threading.Tasks;
+using Formatter = Manager.Formatter;
 
 namespace Service
 {
@@ -12,15 +20,58 @@ namespace Service
     {
         static void Main(string[] args)
         {
+            //srvCertCN.SubjectName should be set to the service's username. .NET Identity class provides information about Windows user running the given process
+            string srvCertCN = Formatter.ParseName(WindowsIdentity.GetCurrent().Name);
+
             NetTcpBinding binding = new NetTcpBinding();
-            string address = "net.tcp://localhost:9999/WCFService";
+            binding.Security.Transport.ClientCredentialType = TcpClientCredentialType.Certificate;
+
+            string address = "net.tcp://localhost:9999/Receiver";
             ServiceHost host = new ServiceHost(typeof(WCFService));
             host.AddServiceEndpoint(typeof(IWCFService), binding, address);
-            host.Open();
-            Console.WriteLine("Service started. \nWaiting for clients...");
-            Console.ReadKey();
-            host.Close();
 
+            //Custom validation mode enables creation of a custom validator - certificateValidator
+            host.Credentials.ClientCertificate.Authentication.CertificateValidationMode = X509CertificateValidationMode.Custom;
+            host.Credentials.ClientCertificate.Authentication.CustomCertificateValidator = new ServiceCertValidator();
+
+            //If CA doesn't have a CRL associated, WCF blocks every client because it cannot be validated
+            host.Credentials.ClientCertificate.Authentication.RevocationMode = X509RevocationMode.NoCheck;
+
+            //Set appropriate service's certificate on the host. Use CertManager class to obtain the certificate based on the "srvCertCN"
+            host.Credentials.ServiceCertificate.Certificate = CertManager.GetCertificateFromStorage(StoreName.My, StoreLocation.LocalMachine, srvCertCN);
+
+            host.Authorization.PrincipalPermissionMode = PrincipalPermissionMode.Custom;
+            List<IAuthorizationPolicy> policies = new List<IAuthorizationPolicy>();
+            //policies.Add(new CustomAuthorizationPolicy());
+            host.Authorization.ExternalAuthorizationPolicies = policies.AsReadOnly();
+
+            ServiceSecurityAuditBehavior newAudit = new ServiceSecurityAuditBehavior();
+            newAudit.AuditLogLocation = AuditLogLocation.Application;
+            newAudit.ServiceAuthorizationAuditLevel = AuditLevel.SuccessOrFailure;
+
+            host.Description.Behaviors.Remove<ServiceSecurityAuditBehavior>();
+            host.Description.Behaviors.Add(newAudit);
+
+            Database.koncerti = Database.ReadKoncerti();
+            Database.rezervacije = Database.ReadRezervacije();
+            Database.korisnici = Database.ReadKorisnici();
+            Database.ReadRacun();
+
+            try
+            {
+                host.Open();
+                Console.WriteLine("WCFService is opened. Press <enter> to finish...");
+                Console.ReadLine();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error", ex.Message);
+                Console.ReadLine();
+            }
+            finally
+            {
+                host.Close();
+            }
         }
     }
 }
--- Service/WCFService.cs
+++ Service/WCFService.cs
@@ -1,33 +1,220 @@
-﻿using Contracts;
+﻿using Common;
+using Manager;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Principal;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Service
 {
     public class WCFService : IWCFService
     {
-        public void DodajKoncert()
+        public void DodajKoncert(int id, string naziv, DateTime vremePocetka, string lokacija, double cenaKarte, out int idKoncerta)
         {
-            throw new NotImplementedException();
+            CustomPrincipal pr = Thread.CurrentPrincipal as CustomPrincipal;
+            idKoncerta = -1;
+
+            if(pr.IsInRole("Admin"))
+            {
+                Console.WriteLine("\nDodaj koncert...");
+                Koncert koncert = null;
+                if(Database.koncerti.Count()>0)
+                {
+                    koncert = new Koncert(Database.koncerti.Count(), naziv, vremePocetka, lokacija, cenaKarte);
+                    idKoncerta = Database.koncerti.Count();
+                }
+                else
+                {
+                    koncert = new Koncert(0, naziv, vremePocetka, lokacija, cenaKarte);
+                    idKoncerta = 0;
+                }
+                Database.koncerti.Add(koncert);
+
+                try
+                {
+                    Audit.AddSuccess("bb", "koncert");
+                }
+                catch(Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+                Database.WriteKoncerti();
+            }
+            else
+            {
+                try
+                {
+                    Audit.AuthorizationFailed("nn", "Dodaj koncert", "Admin moze da dodaje koncerte");
+                }
+                catch(Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+            }
         }
 
-        public void NapraviRezervaciju()
+        public void IzmeniKoncert(int id, string naziv, DateTime vremePocetka, string lokacija, double cenaKarte)
         {
-            throw new NotImplementedException();
+            CustomPrincipal pr = Thread.CurrentPrincipal as CustomPrincipal;
+
+            if (pr.IsInRole("Admin"))
+            {
+                Console.WriteLine("\nIzmena koncerta...");
+
+                for(int i=0; i<Database.koncerti.Count(); i++)
+                {
+                    if(id.Equals(Database.koncerti[i].Id))
+                    {
+                        Database.koncerti[i].Naziv = naziv;
+                        Database.koncerti[i].VremePocetka = vremePocetka;
+                        Database.koncerti[i].Lokacija = lokacija;
+                        Database.koncerti[i].CenaKarte = cenaKarte;
+                    }
+                }
+
+                try
+                {
+                    Audit.ChangeSuccess("NN", "koncert");
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+                Database.WriteKoncerti();
+            }
+            else
+            {
+                try
+                {
+                    Audit.AuthorizationFailed("nn", "Dodaj koncert", "Admin moze da menja koncerte");
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+            }
         }
 
-
-        public void IzmeniKoncert()
+        public void NapraviRezervaciju(int idKoncerta, DateTime vremeRezervacije, int kolicinaKarata, out int idRezervacije)
         {
-            throw new NotImplementedException();
+            CustomPrincipal pr = Thread.CurrentPrincipal as CustomPrincipal;
+            idRezervacije = -1;
+
+            if (pr.IsInRole("Korisnik") || pr.IsInRole("Dobri"))
+            {
+                Console.WriteLine("Napraviti rezervaciju");
+                Rezervacija r = null;
+
+                for (int i = 0; i < Database.korisnici.Count(); i++)
+                {
+                    if (Database.korisnici[i].Username.Equals("nn"))
+                    {
+                        if(Database.rezervacije.Count()>0)
+                        {
+                            r = new Rezervacija(0, idKoncerta, vremeRezervacije, kolicinaKarata);
+                            idRezervacije = 0;
+                        }
+                        Database.korisnici[i].Rezervacije.Add(r);
+
+                        try
+                        {
+                            Audit.AddSuccess("nn", "rezervacije");
+                        }
+                        catch(Exception e)
+                        {
+                            Console.WriteLine(e.Message);
+                        }
+                    }
+                }
+                Database.rezervacije.Add(r);
+                Database.WriteRezervacije();
+                Database.WriteKorisnici();
+            }
+            else
+            {
+                Audit.AuthorizationFailed("bb", "Napravi rezervaciju", "Rezervacija moze biti napravljena samo od grupe Korisnik ili grupe dobri");
+            }
         }
 
-        public void PlatiRezervaciju()
+        public void PlatiRezervaciju(int id)
         {
-            throw new NotImplementedException();
+            CustomPrincipal pr = Thread.CurrentPrincipal as CustomPrincipal;
+
+            if(pr.IsInRole("Korisnik")||pr.IsInRole("Dobri"))
+            {
+                Console.WriteLine("Platiti rezervaciju");
+                foreach(Korisnik k in Database.korisnici)
+                {
+                    if(k.Username.Equals("nn"))
+                    {
+                        foreach(Rezervacija r in k.Rezervacije)
+                        {
+                            foreach(Koncert ko in Database.koncerti)
+                            {
+                                if(ko.Id.Equals(r.Id))
+                                {
+                                    if(r.Id.Equals(r.Id))
+                                    {
+                                        k.Stanje -= r.KolicinaKarata * ko.CenaKarte;
+                                        try
+                                        {
+                                            Audit.ChangeSuccess(Formatter.ParseName(WindowsIdentity.GetCurrent().Name), "stanje");
+                                        }
+                                        catch(Exception e)
+                                        {
+                                            Console.WriteLine(e.Message);
+                                        }
+                                    }
+                                    else
+                                    {
+                                        try
+                                        {
+                                            Audit.ChangeSuccess(Formatter.ParseName(WindowsIdentity.GetCurrent().Name), "stanje");
+                                        }
+                                        catch (Exception e)
+                                        {
+                                            Console.WriteLine(e.Message);
+                                        }
+                                    }
+
+                                    for(int i=0; i<k.Rezervacije.Count(); i++)
+                                    {
+                                        if(k.Rezervacije[i].Id.Equals(r.Id))
+                                        {
+                                            k.Rezervacije[i].Stanje = StanjeRezervacije.PLACENA;
+                                        }
+
+                                        try
+                                        {
+                                            Audit.PaySuccess("bb", StanjeRezervacije.POTREBNO_PLATITI.ToString(), StanjeRezervacije.PLACENA.ToString());
+                                        }
+                                        catch(Exception e)
+                                        {
+                                            Console.WriteLine(e.Message);
+                                        }
+                                        Database.WriteRezervacije();
+                                        Database.WriteKorisnici();
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            else
+            {
+                try
+                {
+                    Audit.AuthorizationFailed("bb", "Plati rezervaciju", "Rezervacija je placena od grupe Korisnik ili grupe Dobri.");
+                }
+                catch(Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+            }
         }
     }
 }
